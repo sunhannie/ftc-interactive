81aa0a58db4f96b0a71c31669d9808d5419c4414
sunhannie <hhy1770547592@163.com>
2018-08-09 16:43:27 +0800

    add views

---
+++ client/components/question/question.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+import questionCss from './question.scss';
+class Question extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    return (
+      <div class="overview-container">
+   
+        <div class="content">
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default Question;
--- client/index.js
+++ client/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 
-// import Nav from './component/nav/nav.js';
+import Overlay from './components/overlay/overlay.js';
+
+import Overview from './components/overview/overview.js';
 // import Login from './component/login/login.js';
 
 class App extends React.Component {
@@ -16,11 +18,12 @@ class App extends React.Component {
   render() {
     return (
     	<div>
-        {/*<Nav />
-        <Login />*/}
-        start
+        <Overview />
+        {/*<Overlay />*/}
+        {/*<Login />*/}
+        {/*start
         <div><span>test</span></div>
-        <button>提交1</button>
+        <button>提交1</button>*/}
       </div>
     );
   }
