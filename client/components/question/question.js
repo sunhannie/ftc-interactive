5807e5089eb1da111f4a1ae5c9345c9c4e64654b
sunhannie <hhy1770547592@163.com>
2018-08-13 10:32:46 +0800

    add input range style

--- client/components/question/question.js
+++ client/components/question/question.js
@@ -7,14 +7,16 @@ class Question extends Component {
   constructor(props) {
     super(props);
     const { questions } = props; // eslint-disable-line
-
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  
+  handleChange(value) {
+
+  }
   render() {
 
     return (
-      <div className={`question`}>
+      <div className="question-container">
         <h2 className="o-typography-subhead--crosshead">
           1.健康保险支出
         </h2>
@@ -22,12 +24,14 @@ class Question extends Component {
         <p className="o-typography-lead--small">
           你认为中国有多少百分比的国内生产总值（GDP）花在医疗保健支出上？
         </p>
-        {/*<div className="tringle"></div>
-        <nav>
-            <a href="#">Home</a>
-            <a href="#">Projects</a>
-            <a href="#">About</a>
-        </nav>*/}
+        <div class="input-section">
+          <div class="input-value">
+            <div class="left-value">0</div>
+            <div class="right-value">100</div>
+          </div>
+          <input type="range" step="1" min="0" max="100" value="50" onChange={event => this.handleChange(event.target.value)}/>
+          <output>50</output>
+        </div>
 
 
 
