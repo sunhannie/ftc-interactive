209dffb6d7a2353a3ea72f1b9030956359e10248
sunhannie <hhy1770547592@163.com>
2018-08-13 11:23:32 +0800

    add legend

--- client/components/question/question.js
+++ client/components/question/question.js
@@ -24,18 +24,33 @@ class Question extends Component {
         <p className="o-typography-lead--small">
           你认为中国有多少百分比的国内生产总值（GDP）花在医疗保健支出上？
         </p>
-        <div class="input-section">
-          <div class="input-value">
-            <div class="left-value">0</div>
-            <div class="right-value">100</div>
-          </div>
-          <input type="range" step="1" min="0" max="100" value="50" onChange={event => this.handleChange(event.target.value)}/>
-          <output>50</output>
+        <div  class="input-section">
+          <form>
+            <div class="input-value">
+              <div class="left-value">0</div>
+              <div class="right-value">100</div>
+            </div>
+            <input type="range" step="1" min="0" max="100" value="50" onChange={event => this.handleChange(event.target.value)}/>
+            <output>50</output>
+          </form>
+          <div></div>
         </div>
+        <div class="look-answer"><button>看答案</button> </div>
 
+        <div class="reslut-container">
+          <div class="legend">
+            <div class="actual-situation"><span class="circle"></span><span>实际情况</span></div>
+            <div class="response"><span class="circle"></span><span>你的回答</span></div>
+            <div class="survey"><span class="circle"></span><span>问卷调查结果</span></div>
+          </div>
 
-
-      </div>
+          <div class="percentage">
+            <div class="answer"><span>答案是</span><span>6%</span></div>
+            <div class="thinking"><span>你认为是</span><span>52%</span></div>
+            <div class="survey-people"><span>调查民众答案</span><span>21%</span></div>
+          </div>
+        </div>
+       </div>
     );
   }
 }
